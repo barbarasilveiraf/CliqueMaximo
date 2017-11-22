26b8f112634ed6f71e51c2bc836b8f032ef6b754
Bárbara <13971769+barbarasilveiraf@users.noreply.github.com>
2017-11-22 16:04:27 -0200

    arquivo para realizar testes mais rapido

---
+++ execucaoTestes.py
@@ -0,0 +1,24 @@
+import os
+from subprocess import Popen, PIPE
+
+#leitura do diretorio
+path = "C:/Users/Barbara/PycharmProjects/Testes_TP_Final_PAA/"
+files = [f for f in os.listdir(path) if f.endswith('.edges')]
+
+file = open("resultadoTestes.txt", "w")
+
+#executar o programa
+for f in files:
+    #os.system('python cliqueMaximo.py '+ (path+f))
+    #script_path = os.path.join("python", 'cliqueMaximo.py '+ (path+f))
+    #p = Popen([sys.executable, '-u', script_path],stdout=PIPE, stderr=STDOUT, bufsize=1)
+    for x in range(1, 11): #executar 10 vezes cada arquivo
+        pipe = Popen('python '+ 'cliqueMaximo.py '+ (path+f), shell=True, stdout=PIPE).stdout
+        output = pipe.read()
+        texto = str(output).replace(r"\r\n", ";").replace("b'", "").replace("'", "")
+        file.write(texto)
+        file.write("\n")
+
+file.close()
+
+
