b38ee1faf8d7c651dde8446a2420ed42c91f91f4
Barbara <barbarasilveiraf@gmail.com>
2017-11-24 21:50:15 -0200

    dados arquivo

--- cliqueMaximo.py
+++ cliqueMaximo.py
@@ -25,19 +25,26 @@ def inicio():
     global Grafo
     global mediaGrauVertices
     global desvioPadrao
+    end = None
     #arq = open(str(sys.argv[0]), 'rb')
-    nomeCaminhoArq = 'C:/Users/Barbara/Dropbox/UFMG/PAA/Projeto Final/Entrega 2/testes preliminares/bases/348.edges'
-    arq = open(nomeCaminhoArq, 'rb')
+    #nomeCaminhoArq = 'C:/Users/Barbara/Dropbox/UFMG/PAA/Projeto Final/Entrega 2/testes preliminares/bases/348.edges'
+    #arq = open(nomeCaminhoArq, 'rb')
+    arq = open(str(sys.argv[1]), 'rb')
 
+    nomeArq = str(sys.argv[1])[42:end]
+
+    # print("1 Arquivo")
+    print(nomeArq)
+
+    # Leitura do arquivo
     G = nx.read_edgelist(arq)
 
     #para cada vertice, vou adicionar a aresta com vértice 0 - orientacao da base
-    end = None
-    nomeArq = nomeCaminhoArq[84:end].replace(".edges","")
+    nomeArqVertice = nomeArq.replace(".edges", "")
 
     vertices = [x for x in nx.nodes(G)]
     for n in vertices:
-        G.add_edge(nomeArq, n)
+        G.add_edge(nomeArqVertice, n)
 
     Grafo = G
 
@@ -59,6 +66,27 @@ def inicio():
 
     #calcular o desvio padrao
     desvioPadrao = desvio_padrao(mediaGrauVertices, grafosList)
+
+    # print("2 Vertice Orign")
+    print(nx.number_of_nodes(G))
+
+    # print("3 Arestas Orign")
+    print(nx.number_of_edges(G))
+
+    # print("4 media")
+    print(mediaGrauVertices)
+
+    # print("5 Densidade")
+    print("%.4f" % nx.density(G))
+
+    # print("6 Vertice Utiliz")
+    print(nx.number_of_nodes(G))
+
+    # print("7 Arestas Orign")
+    print(nx.number_of_edges(G))
+
+    # print("8 Desvio Padrao")
+    print(desvioPadrao)
 
     #remove os elementos cujo grau menor que a media
     #for n in vertices:
@@ -93,19 +121,22 @@ def clique(S, tamanho, cliquesTemp):
             vizinhosVertices = [x for x in Grafo.neighbors(i) if Grafo.degree[x] >= mediaGrauVertices]
             clique(list(set(S).intersection(set(list(vizinhosVertices)))), tamanho+1, cliquesTemp)
 
-#h = hpy()
-#h.setrelheap()
+h = hpy()
+h.setrelheap()
 
-print("HORA ATUAL " + str(datetime.now().hour )+ ":" + str(datetime.now().minute) +":" + str(datetime.now().second))
+#print("HORA ATUAL " + str(datetime.now().hour )+ ":" + str(datetime.now().minute) +":" + str(datetime.now().second))
 tempoInicio = time.time()
 lista = [x[0] for x in inicio()]
 clique(lista, 0, [])
-print('CLIQUE MAX - ' + str(max))
+# print('9 CLIQUE MAX ')
+print(str(max))
 #print('Elementos MAX - ' + str(elementosClique))
 #cliques = list(find_cliques(Grafo))
 tempoFim = time.time()
-print("TEMPO -> " + "%.4f" % (tempoFim - tempoInicio))
+# print("10 Tempo")
+print("%.4f" % (tempoFim - tempoInicio))
 
-#x = h.heap() #depois do coigo
-#print('memoria: '+str(x.size))
-#print ('memoria'+ str(x))
+x = h.heap()  # depois do coigo
+
+# print("11 Memoria")
+print(str(x.size))
