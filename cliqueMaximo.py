00e79038f802e17e6752994bf79a01f7c8205fb5
Barbara <barbarasilveiraf@gmail.com>
2017-11-24 20:31:46 -0200

    dados pra imprimir na tela

--- cliqueMaximo.py
+++ cliqueMaximo.py
@@ -1,8 +1,9 @@
 import networkx as nx
 import time
 from datetime import datetime
+from guppy import hpy
+import os
 import sys
-#from guppy import hpy
 
 from networkx.algorithms.clique import find_cliques
 
@@ -10,25 +11,64 @@ Grafo = nx.Graph
 max = 0
 elementosClique = []
 
+
 def inicio():
     global Grafo
-    #arq = open('C:/Users/Barbara/PycharmProjects/TP_Final_PAA/facebook/1684.edges', 'rb')
-    #arq = open(str(sys.argv[0]), 'rb')
-    arq = open('C:/Users/Barbara/Dropbox/UFMG/PAA/Projeto Final/Entrega 2/testes preliminares/bases/3980.edges', 'rb')
-    #print("sys.argv[0] " + sys.argv[0])
+    end = None
+    # nomeCaminhoArq = 'C:/Users/Barbara/Dropbox/UFMG/PAA/Projeto Final/Entrega 2/testes preliminares/bases/348.edges'
+    # arq = open('C:/Users/Barbara/PycharmProjects/TP_Final_PAA/facebook/1684.edges', 'rb')
+    # print("oii")
+    # arq = open('graph_v100_a495.edges', 'rb')
+    arq = open(str(sys.argv[1]), 'rb')
+
+    nomeArq = str(sys.argv[1])[42:end]
+
+    # print("1 Arquivo")
+    print(nomeArq)
+
+    # Leitura do arquivo
     G = nx.read_edgelist(arq)
 
-    #para cada vértice, vou adicionar a aresta com vértice 0 - orientacao da base
+    # para cada vertice, vou adicionar a aresta com vertice 0 - orientacao da base
+
+
+    nomeArqVertice = nomeArq.replace(".edges", "")
+
     vertices = [x for x in nx.nodes(G)]
     for n in vertices:
-        G.add_edge('3980', n)
+        G.add_edge(nomeArqVertice, n)
 
-    #Congela Grafo
+    # print("2 Vertice Orign")
+    print(nx.number_of_nodes(G))
+
+    # print("3 Arestas Orign")
+    print(nx.number_of_edges(G))
+
+    # print("4 media")
+    print("0.00")
+
+    # print("5 Densidade")
+    print("%.4f" % nx.density(G))
+
+    # print("6 Vertice Utiliz")
+    print(nx.number_of_nodes(G))
+
+    # print("7 Arestas Orign")
+    print(nx.number_of_edges(G))
+
+    # print("8 Desvio Padrao")
+    print("0.00")
+
+    # print((nx.is_connected(G)))
+
+    # print(psutil.virtual_memory())
+
+    # Congela Grafo
     nx.freeze(G)
 
     Grafo = G
 
-    #Lista de tuplas(vertice, grau)
+    # Lista de tuplas(vertice, grau)
     grafosList = list(G.degree)
 
     #ordenacao DESCRESCENTE pelo grau
@@ -36,43 +76,48 @@ def inicio():
 
     return grafosList
 
+
 def clique(S, tamanho, cliquesTemp):
     global Grafo
     global max
     global elementosClique
 
-    if(len(S) == 0):
-        if(tamanho > max):
+    if (len(S) == 0):
+        if (tamanho > max):
             max = tamanho
-            elementosClique = cliquesTemp[-max:]; #pego os max elementos para tras, pois eles fazem parte do meu clique
+            elementosClique = cliquesTemp[
+                              -max:];  # pego os max elementos para tras, pois eles fazem parte do meu clique
         return
-    while(len(S) != 0):
-        if(tamanho + len(S) <= max):
+    while (len(S) != 0):
+        if (tamanho + len(S) <= max):
             return
         i = S[0]
         S.remove(i)
         cliquesTemp.append(i);
-        clique(list(set(S).intersection(set(list(Grafo.neighbors(i))))), tamanho+1, cliquesTemp)
+        clique(list(set(S).intersection(set(list(Grafo.neighbors(i))))), tamanho + 1, cliquesTemp)
 
 
+h = hpy()
+h.setrelheap()
 
-#h = hpy()
-#h.setrelheap()
-
-print("HORA ATUAL " + str(datetime.now().hour )+ ":" + str(datetime.now().minute) +":" + str(datetime.now().second))
+# print("HORA ATUAL " + str(datetime.now().hour )+ ":" + str(datetime.now().minute) +":" + str(datetime.now().second))
 tempoInicio = time.time()
 lista = [x[0] for x in inicio()]
 clique(lista, 0, [])
-print('CLIQUE MAX - ' + str(max))
-#print('Elementos MAX - ' + str(elementosClique))
-#cliques = list(find_cliques(Grafo))
+# print('9 CLIQUE MAX ')
+print(str(max))
+# print('Elementos MAX - ' + str(elementosClique))
+# cliques = list(find_cliques(Grafo))
+# l = [len(x) for x in cliques]
+# print('Outro: ' + str(sorted(l)[-1:]))
 tempoFim = time.time()
-print("TEMPO -> " + "%.4f" % (tempoFim - tempoInicio))
+# print("10 Tempo")
+print("%.4f" % (tempoFim - tempoInicio))
 
-#x = h.heap() #depois do coigo
-#print('memoria: '+str(x.size))
-#print ('memoria'+ str(x))
+x = h.heap()  # depois do coigo
+# print('memoria: '+str(x.size))
 
-#print(psutil.virtual_memory())
-#print(psutil.swap_memory())
+# print("11 Memoria")
+print(str(x.size))
 
+# print ('memoria'+ str(x))
